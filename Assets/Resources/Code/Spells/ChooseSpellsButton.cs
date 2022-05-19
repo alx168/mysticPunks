8c3bcd1a34ca2ebb0013a72b7614c53789a71deb
alx168 <alx168@gmail.com>
2022-05-19 01:43:47 -0400

    choose spells button wip

---
+++ Assets/Resources/Code/Spells/ChooseSpellsButton.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class ChooseSpellsButton : MonoBehaviour
+{
+    [SerializeField]
+    private Player player;
+    [SerializeField]
+    private Text spellsText;
+
+
+    public void onClick() {
+     Debug.Log("hi");   
+    }
+   
+}
